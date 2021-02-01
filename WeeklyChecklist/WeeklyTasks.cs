502b8649f6845df40913ea87f0d3d0dd748ed8c2
dtlvoigt <dtlvoigt@gmail.com>
2021-02-01 12:21:11 -0800

    Created first classes and task struct

--- WeeklyChecklist/WeeklyTasks.cs
+++ WeeklyChecklist/WeeklyTasks.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace WeeklyChecklist
 {
+    //determines if debug info should be shown while running application
+    static class Debug
+    {
+        public static bool debug = false;
+    }
+
+    //holds info about tasks such as name, count, date completed...
+    struct Task
+    {
+
+    }
+    //holds all tasks
+    class TaskList
+    {
+
+    }
+
     class WeeklyTasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            //user wants debug information
+            if (args[1].ToLower() == "debug")
+                Debug.debug = true;
+
+            Console.WriteLine(Debug.debug);
             Console.Read();
         }
     }
